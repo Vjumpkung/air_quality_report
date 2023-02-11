5be897819856210c34b7339ac734b27da9db2998
vjumpkung <vjumpkung@gmail.com>
2023-02-11 13:01:14 +0700

    fix uri

--- notification.py
+++ notification.py
@@ -5,7 +5,7 @@ import urllib
 import os
 
 load_dotenv(".env")
-REDIRECT_URI_NOTIFY = urllib.parse.quote(os.getenv("REDIRECT_URI_NOTIFY"))
+REDIRECT_URI_NOTIFY = os.getenv("REDIRECT_URI_NOTIFY")
 CLIENT_ID_NOTIFY = urllib.parse.quote(os.getenv("CLIENT_ID_NOTIFY"))
 CLIENT_SECRET_NOTIFY = urllib.parse.quote(os.getenv("CLIENT_SECRET_NOTIFY"))
 
@@ -19,25 +19,27 @@ def get_access_token(code):
     redirect_uri = REDIRECT_URI_NOTIFY
     client_id = CLIENT_ID_NOTIFY
     client_secret = CLIENT_SECRET_NOTIFY
-    headers = {'Content-Type': content_type}
+    headers = {"Content-Type": content_type}
     data = {
         "grant_type": grant_type,
         "code": code,
         "redirect_uri": redirect_uri,
         "client_id": client_id,
-        "client_secret": client_secret
+        "client_secret": client_secret,
     }
     response = requests.post(api_url, headers=headers, data=data)
-    return response.json()['access_token']
+    return response.json()["access_token"]
 
 
 def send_notification(message, token):
     """Send notification from message anf token provided."""
-    url = 'https://notify-api.line.me/api/notify'
+    url = "https://notify-api.line.me/api/notify"
     if not token:
         return None
-    headers = {'content-type': 'application/x-www-form-urlencoded',
-               'Authorization': 'Bearer ' + token}
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "Authorization": "Bearer " + token,
+    }
 
-    response = requests.post(url, headers=headers, data={'message': message})
-    return response.json()['status']
+    response = requests.post(url, headers=headers, data={"message": message})
+    return response.json()["status"]
